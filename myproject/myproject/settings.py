670db330bbfc754779013347611d4ba18d4c0eca
Matheus Meissner <matheus.iembo@gmail.com>
2025-01-12 18:42:31 -0300

    atualiza_arquivos

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "corsheaders.middleware.CorsMiddleware",
-
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = "myproject.urls"
@@ -110,7 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/fernando10092/Project--Twitter/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
